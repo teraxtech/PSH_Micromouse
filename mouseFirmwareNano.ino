faa3b005368181608099d6fda9b29a5a216e85d5
teraxtech <teraxtech@gmail.com>
2026-02-20 18:19:49 -0500

    Implemented motor control as a class

--- mouseFirmwareNano.ino
+++ mouseFirmwareNano.ino
@@ -12,30 +12,74 @@
 
 volatile int direction = 1; //1=clockwise, -1 = counterClockwise
 int rotationThreshhold = 1;
-struct motor_state {
-  short pin_cw;
-  short pin_ccw;
-  short hall_pin_a;
-  short hall_pin_b;
-  bool direction;
-  long revolutions;
+class Motor {
+  
+  static void interrupt1(){
+  }
+  private:
+    short pin_cw;
+    short pin_ccw;
+    short hall_pin_a;
+    short hall_pin_b;
+    int hall_a;
+    int hall_b;
+    bool direction;
+    long revolutions;
+    long target = 0;
+
+  public:
+    Motor(short cw, short ccw, short a, short b){
+        pin_cw = cw;
+        pin_ccw = ccw;
+        hall_pin_a = a;
+        hall_pin_b = b;
+
+        pinMode(cw , OUTPUT);
+        pinMode(ccw , OUTPUT);
+        pinMode(a  , INPUT);
+        pinMode(b , INPUT);
+    }
+
+    void readHall(){
+      bool hall_a = digitalRead(this->hall_pin_a);
+      bool hall_b = digitalRead(this->hall_pin_b);
+      
+      this->hall_a = hall_a;
+      this->hall_b = hall_b;
+      short direction = hall_b?-1:1;
+
+
+      this->revolutions += direction;
+      if((this->target-this->revolutions)*direction>0)return;
+
+      if(direction == 1){
+        digitalWrite(this->pin_cw, LOW);
+      }else{
+        digitalWrite(this->pin_ccw, LOW);
+      }
+    }
+
+    void turn(int amount){
+      this->target+= amount;
+      if(amount > 0){
+        digitalWrite(this->pin_cw, HIGH);
+      }else{
+        digitalWrite(this->pin_ccw, HIGH);
+      }
+    }
+
+    int get_pin_a(){ return this->hall_pin_a; }
+
+    int getRevolutions(){ return this->revolutions; }
+    
+    // void addInterrupt(void* callback()){
+    //   attachInterrupt(digitalPinToInterrupt(this->hall_a), [callback]{ callback(); }, RISING);
+    // }
 };
 
-volatile motor_state motor1 = {7,6,2,4, true, 0};
-volatile motor_state motor2 = {9,8,3,5, true, 0};
-volatile int count = 0;
-
+volatile Motor left_motor = Motor(16,17,3,5);
 
 long int programCounter = 0;
-
-long getRevolution(volatile motor_state &state);
-
-void handleHallTrigger_motor1(){
-  motor1.revolutions += getRevolution(motor1);
-}
-void handleHallTrigger_motor2(){
-  motor2.revolutions += getRevolution(motor2);
-}
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -46,103 +90,25 @@ void setup() {
 
   //set the resolution to 12 bits (0-4095)
   // analogReadResolution(12);;
-  pinMode(motor1.pin_cw , OUTPUT);
-  pinMode(motor1.pin_ccw , OUTPUT);
-  pinMode(motor2.pin_cw , OUTPUT);
-  pinMode(motor2.pin_ccw , OUTPUT);
-  //
-  //
-  pinMode(motor1.hall_pin_a  , INPUT);
-  pinMode(motor1.hall_pin_b , INPUT);
-  pinMode(motor2.hall_pin_a  , INPUT);
-  pinMode(motor2.hall_pin_b , INPUT);
+  const Motor right_motor = Motor(14,15,2,4);
 
-  attachInterrupt(digitalPinToInterrupt(motor1.hall_pin_a), handleHallTrigger_motor1, HIGH);
-  attachInterrupt(digitalPinToInterrupt(motor2.hall_pin_a), handleHallTrigger_motor2, HIGH);
+  // right_motor.attachInterrupt(right_motor.readHall);
+  attachInterrupt(digitalPinToInterrupt(right_motor.get_pin_a()), [right_motor]{ right_motor.readHall(); }, RISING);
+  // attachInterrupt(digitalPinToInterrupt(left_motor.get_pin_a()), []{ left_motor.readHall(); }, RISING);
+
+  right_motor.turn(1000);
 }
 
-
-long getRevolution(volatile motor_state &state){
-  int sensorValue1 = digitalRead(state.hall_pin_a);
-  int sensorValue2 = digitalRead(state.hall_pin_b);
-  bool hall_a = sensorValue1 == HIGH;
-  bool hall_b = sensorValue2 == HIGH;
-
-  
-  // if(past_hall_a == hall_a && past_hall_b == hall_b) return previous_count;
-  // if(hall_a == 0) return 0;
-  bool clockwise = hall_b;
-
-  motor_state.hall_a = hall_a
-  motor_state.hall_b = hall_b
-  Serial.println(clockwise);
-
-
-  return clockwise?-1:1;
-}
-
+long int timeOfLastPrint = 0;
+char output_buf[50];
 // the loop routine runs over and over again forever:
 void loop() {
   programCounter++;
 
-
-  int timestep = 4000;
-  if(programCounter%timestep == 0){
-    if(programCounter%(2*timestep) == 0*timestep)direction = -1;
-    if(programCounter%(2*timestep) == 1*timestep)direction = 1;
-  }
-
-  // read the input on analog pin 0:
-  int sensorValue1 = digitalRead(motor1.hall_pin_a);
-  int sensorValue2 = digitalRead(motor1.hall_pin_b);
-
-  // count = countRotations(previous_state, count, sensorValue1, sensorValue2);
-
-  // previous_state.motor1a = false;
-
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  // float voltage1 = sensorValue1 * (5.0 / 1023.0);
-  // float voltage2 = sensorValue2 * (5.0 / 1023.0);
-  // print out the value you read:
-
-  char output_buf[50];
-  if (programCounter%100==0){
-    // sprintf(output_buf, "dir: %2d, val1: %4d, val2: %4d", direction, sensorValue1, sensorValue2);
-    // Serial.print(output_buf);
-    // Serial.print(", count = ");
-    // Serial.print(motor1.revolutions);
-    // Serial.print(", count = ");
-    // Serial.print(count);
-    // Serial.print(", clock = ");
-    // Serial.println(programCounter);
-  }
-
-  // if((sensorValue1 - 512) * rotationThreshhold > 400){
-  //   count+=direction;
-  //   rotationThreshhold*=-1;
+  // if(millis() - timeOfLastPrint > 100){
+  //   sprintf(output_buf, "dir: %2d, val1: %4d, val2: %4d \n", 0, right_motor.getRevolutions(), left_motor.getRevolutions());
+  //   Serial.println(output_buf);
+  //   timeOfLastPrint = millis();
   // }
 
-
-  switch(direction){
-    case 1:
-      digitalWrite(motor2.pin_ccw, LOW);
-      if(motor2.revolutions<3000){
-        digitalWrite(motor2.pin_cw, HIGH);
-      }else{
-        digitalWrite(motor2.pin_cw, LOW);
-      }
-      break;
-    case 0:
-      digitalWrite(motor2.pin_cw, LOW);
-      digitalWrite(motor2.pin_ccw, LOW);
-      break;
-    case -1:
-      digitalWrite(motor2.pin_cw, LOW);
-      if(motor2.revolutions>0){
-        digitalWrite(motor2.pin_ccw, HIGH);
-      }else{
-        digitalWrite(motor2.pin_ccw, LOW);
-      }
-      break;
-  }
 }
